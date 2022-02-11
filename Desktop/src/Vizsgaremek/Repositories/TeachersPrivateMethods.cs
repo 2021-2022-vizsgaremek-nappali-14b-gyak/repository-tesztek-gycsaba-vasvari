90e6d8a0b392fdd76601dc9fa389b5dff036017f
Csaba Gyuris <gycsaba@vasvari.hu>
2022-02-11 13:04:04 +0100

    Fájl a privát metódusoknak

---
+++ Desktop/src/Vizsgaremek/Repositories/TeachersPrivateMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Vizsgaremek.Models;
+using Vizsgaremek.Stores;
+using Vizsgaremek.Repositories.Interface;
+
+namespace Vizsgaremek.Repositories
+{
+    public partial class Teachers : IRepositoryAPIStringId<Teacher>
+    {
+    }
+}
